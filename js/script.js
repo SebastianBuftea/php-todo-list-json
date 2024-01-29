0430dfebd5d894c6dd731e958f72b5fbab2a3532
SebastianBuftea <sebibuftea85@gmail.com>
2024-01-29 15:34:06 +0100

    [ADD] function of add a todo and to reed the todolist in javascript

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            todoList: ['lavare', 'pulire', 'cucinare'],
+            todoItem: '',
         }
 
     },
     methods: {
-
+        addToDo() {
+            this.todoList.push(this.todoItem)
+            this.todoItem = ''
+        }
     }
 }).mount("#app")
